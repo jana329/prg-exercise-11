90de5068edef43b820bc03cdfc4bbdb41095290d
Jana Miksikova <265827@vutbr.cz>
2026-04-23 15:41:47 +0200

    ctvrty

--- sorting.py
+++ sorting.py
@@ -1,4 +1,5 @@
 import random
+import matplotlib.pyplot as plt
 
 def random_numbers(count, low=0, high=100):
     return [random.randint(low, high) for _ in range(count)]
@@ -19,13 +20,30 @@ def selection_sort(seznam_cisel):
     return seznam_kopie
 
 def bubble_sort(seznam_cisel):
+
     seznam_kopie = seznam_cisel.copy()
     n = len(seznam_cisel)
 
+    plt.ion()
+    plt.show()
+
     for i in range(n):
         for j in range(0, n - i - 1):
+            index_highlight1 = j
+            index_highlight2 = j + 1
+            colors = ["steelblue"] * len(seznam_kopie)
+            colors[index_highlight1] = "tomato"
+            colors[index_highlight2] = "tomato"
+            plt.clf()
+            plt.bar(range(len(seznam_kopie)), seznam_kopie, color=colors)
+            plt.title("Bubble Sort")
+            plt.pause(0.1)
             if seznam_kopie[j] > seznam_kopie[j + 1]:
+
                 seznam_kopie[j], seznam_kopie[j + 1] = seznam_kopie[j + 1], seznam_kopie[j]
+
+    plt.ioff()
+    plt.show()
 
     return seznam_kopie
 
@@ -36,8 +54,6 @@ if __name__ == "__main__":
 
     # small = random_numbers(5, low=0, high=20)  # 5 čísel v rozsahu 0–20
 
-    print(selection_sort(random_numbers(20)))
-
     test_kratky = [5, 1, 4, 2, 8]
     test_nahodny = random_numbers(20)
 
@@ -47,5 +63,7 @@ if __name__ == "__main__":
     print(f"random pred serazenim: {test_nahodny}")
     print(f"random po serazeni: {selection_sort(test_nahodny)}")
 
+    bubble = bubble_sort(random_numbers(10))
+    print(bubble)
 
 
