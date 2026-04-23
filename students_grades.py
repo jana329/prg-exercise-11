820c3f238ac19a8cff5731077ce01134c94974f9
Jana Miksikova <265827@vutbr.cz>
2026-04-23 16:08:49 +0200

    sesty

--- students_grades.py
+++ students_grades.py
@@ -8,9 +8,28 @@ class StudentsGrades:
     def count(self):
         return len(self.scores)
 
+    def get_grade(self, index):
+        body = self.get_by_index(index)
+
+        if body >= 90:
+            return "A"
+        elif body >= 80:
+            return "B"
+        elif body >= 70:
+            return "C"
+        elif body >= 60:
+            return "D"
+        elif body >= 50:
+            return "E"
+        else:
+            return "F"
+
 if __name__ == "__main__":
     results = StudentsGrades([85, 42, 91, 67, 50, 73, 100, 38, 58])
 
     print(results.count())  # 9
     print(results.get_by_index(2))  # 91
     print(results.scores)  # [85, 42, 91, 67, 50, 73, 100, 38, 58]
+
+    print(f"{results.get_grade(2)}")
+    print(f"{results.get_grade(7)}")
